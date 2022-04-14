149580188b3aa00a62ca593a56addaa1c667260b
Tereza Malenická <78638738+tereza-tech@users.noreply.github.com>
2022-04-14 23:47:41 +0200

    initial commit

--- src/modules/really_large_app/index.js
+++ src/modules/really_large_app/index.js
@@ -4,3 +4,8 @@ const chosenColor = colors.getRandomColor();
 console.log(
   `You should use ${chosenColor.name} on your website. It's HTML code is ${chosenColor.code}`
 );
+
+const favoriteColor = colors.getBlue();
+console.log(
+  `My favorite color is ${favoriteColor.name}/${favoriteColor.code}, btw`
+);
